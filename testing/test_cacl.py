9e727808ac6f6d0a11785c87efee92268a6559a3
huanhuan <529308803@qq.com>
2020-09-09 14:53:14 +0800

    aaabbb

---
+++ testing/test_cacl.py
@@ -0,0 +1,20 @@
+__author__ = 'zenghuan'
+import pytest
+import allure
+from python import cacl
+
+
+class  TestCacl:
+
+    def setUp(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_div(self):
+        pass
+
+    def tearDown(self):
+        pass
+
