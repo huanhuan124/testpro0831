b21e621842bfe43d733177dbf95d11b1424afc3b
huanhuan <529308803@qq.com>
2020-09-10 16:02:47 +0800

    aaabbb

--- python_test/test_pytest.py
+++ python_test/test_pytest.py
@@ -3,6 +3,12 @@ __author__ = 'zenghuan'
 
 import pytest
 class Test_demo():
+
+    # @pytest.mark.run(order=3)
+    def test_case3(self):
+        print("开始执行testcase3 ***")
+
+    # @pytest.mark.run(order=1)
     def test_case1(self):
         print("开始执行testcase1 ***")
         # assert 'h' in "this"
@@ -11,24 +17,34 @@ class Test_demo():
         pytest.assume( 'a' in "abc")
         pytest.assume( 'a' in "1111")
 
+    def test_case1_1(self):
+        print("开始执行testcase1 ***")
+        # assert 'h' in "this"
+        pytest.assume( 'h' in "this")
+        pytest.assume(1==2)
+        pytest.assume( 'a' in "abc")
+        pytest.assume( 'a' in "1111")
+
+    # @pytest.mark.run(order=2)
     def test_case2(self):
         print("开始执行testcase2 ***")
         assert 'h' in "AA"
 
 
-    def test_case3(self):
-        print("开始执行testcase3 ***")
+
 
 class Test_demo1():
+
+    # @pytest.mark.order2
+    def test_two(self):
+        print("开始执行testtwo ***")
+        assert 'h' in "32"
+    # @pytest.mark.order1
     def test_one(self):
         print("开始执行testone ***")
         assert 'h' in "this"
 
-    def test_two(self):
-        print("开始执行testtwo ***")
-        assert 'h' in "32"
-
-
+    # @pytest.mark.order
     def test_three(self):
         print("开始执行testthree ***")
 
