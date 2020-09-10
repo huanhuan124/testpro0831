7143432f053dee0a638d329830e9b35783699acb
huanhuan <529308803@qq.com>
2020-09-10 15:59:41 +0800

    aaabbb

---
+++ python_test/abc_login.py
@@ -0,0 +1,14 @@
+__author__ = 'zenghuan'
+
+def test_login():
+    print("通过pytest.ini文件改过规则的abc开头的文件")
+
+class HGWZtest:
+    def case01(self):
+        print("pytest.ini case01")
+
+    def test02(self):
+        print("test02")
+
+    def case02(self):
+        print("pytest.ini case02")
