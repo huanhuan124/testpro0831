cfc1423d523eef1e8321029754fa531bbaf294ce
huanhuan <529308803@qq.com>
2020-09-10 16:03:19 +0800

    aaabbb

--- python_test/conftest.py
+++ python_test/conftest.py
@@ -4,3 +4,15 @@ import pytest
 @pytest.fixture()
 def login():
     print("这是登录模块，放在conftest文件中")
+
+
+def pytest_collection_modifyitems(session, config, items):
+    # print(items)
+    # print(type(items))
+    items.reverse()
+    # print(items)
+    # for item in items:
+    #     if "case1" in item.nodeid:
+    #         item.add_marker(pytest.mark.case1)
+    #     elif "one" in item.nodeid:
+    #         item.add_marker(pytest.mark.one)
